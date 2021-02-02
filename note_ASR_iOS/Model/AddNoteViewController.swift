6fd08520338c4024a3cd73850024165ff95bec44
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-02-02 13:06:21 -0500

    load func

--- note_ASR_iOS/Model/AddNoteViewController.swift
+++ note_ASR_iOS/Model/AddNoteViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class AddNoteViewController: UIViewController {
 
+    
+    var selectedSubject : String?
+    var selectedNote : Note?
+    let context =
+        (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var buttonSubject: UIButton!
+    @IBOutlet weak var noteTitle: UITextField!
+    @IBOutlet weak var noteDesc: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    //MARK: - Functions
     
+    func loadSavedData(note:Note?){
+        if let note = note{
+            noteTitle.text = note.title
+            noteDesc.text = note.subDescription
+            selectedNote = note
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+}
 }
