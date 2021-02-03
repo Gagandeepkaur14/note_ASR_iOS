83e7ca2b731393709cff79eaf7c420478d099c6f
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-02-03 09:04:57 -0500

    next step

--- extensions.swift
+++ extensions.swift
@@ -11,11 +11,17 @@ extension UIViewController {
                          subtitle:String? = nil,
                          actionTitle:String? = "Add",
                          cancelTitle:String? = "Cancel",
-                         inputPlaceholder:String? = nil ) {
+                         defaultText:String? = nil,
+                         inputPlaceholder:String? = nil,
+                         inputKeyboardType:UIKeyboardType = UIKeyboardType.default,
+                         cancelHandler: ((UIAlertAction) -> Swift.Void)? = nil,
+                         actionHandler: ((_ text: String?) -> Void)? = nil) {
        
         let alert = UIAlertController(title: title, message: subtitle, preferredStyle: .alert)
         alert.addTextField { (textField:UITextField) in
             textField.placeholder = inputPlaceholder
+            textField.keyboardType = inputKeyboardType
+            textField.text = defaultText
     }
 }
 }
