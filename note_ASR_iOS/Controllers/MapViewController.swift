3f82c4c89c5be7065c684fd051b18e7746b539c9
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-02-03 00:36:46 -0500

    map outlet added

---
+++ note_ASR_iOS/Controllers/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  note_ASR_iOS
+//
+//  Created by parneet sandhu on 2021-02-03.
+//
+
+import UIKit
+import MapKit
+var note : Note?
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadSavedData()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
