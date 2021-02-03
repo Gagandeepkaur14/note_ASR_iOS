d1cdb8c81ed211f050374f466037cabf767507e6
BaljeetAmrit <72460561+AmritKaur0955@users.noreply.github.com>
2021-02-02 21:00:05 -0500

    photo pressed action added

--- note_ASR_iOS/Model/AddNoteViewController.swift
+++ note_ASR_iOS/Model/AddNoteViewController.swift
@@ -7,14 +7,18 @@
 
 import UIKit
 
-class AddNoteViewController: UIViewController {
+class AddNoteViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate  {
 
-    
+    var selectedImage : String?
     var selectedSubject : String?
     var selectedNote : Note?
     let context =
         (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+   
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var buttonSubject: UIButton!
     @IBOutlet weak var noteTitle: UITextField!
     @IBOutlet weak var noteDesc: UITextView!
@@ -33,4 +37,39 @@ class AddNoteViewController: UIViewController {
         }
 
 }
+    
+    //MARK: - IB Actions
+
+    @IBAction func photoPressed(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: UIAlertAction.Style.default, handler: { (alert:UIAlertAction!) -> Void in
+            self.camera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Gallery", style: UIAlertAction.Style.default, handler: { (alert:UIAlertAction!) -> Void in
+            self.photoLibrary()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    func camera()
+    {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        self.present(imagePicker, animated: true)
+        
+    }
+    func photoLibrary()
+    {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true)
+    }
 }
+
