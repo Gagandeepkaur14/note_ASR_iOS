ad3bcbb52de3a02ae2bfb7da99e9377da19f2ae9
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-02-03 22:20:08 -0500

    map added

--- note_ASR_iOS/Controllers/MapViewController.swift
+++ note_ASR_iOS/Controllers/MapViewController.swift
@@ -20,18 +20,35 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadSavedData()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadSavedData(){
+        if let selectedNote = note{
+            
+            let cor = CLLocationCoordinate2D(latitude: selectedNote.lat, longitude: selectedNote.long)
+            
+            // define span
+            let span = MKCoordinateSpan(latitudeDelta: 0.10, longitudeDelta: 0.10)
+            
+            // define the region
+             let region = MKCoordinateRegion(center: cor, span: span)
+            
+            // set the region for the map
+             mapView.region = region
+            
+            //  define annotation
+            let ann = MKPointAnnotation()
+            ann.coordinate = cor
+            ann.title = selectedNote.title
+            ann.subtitle = (selectedNote.date)!.description
+            mapView.addAnnotation(ann)
+        }
+       
     }
-    */
+    
+
+   
 
 }
